918c88620c21d4f8630e9e178c4740e49ed38bba
cnobre <carolinanobre@gmail.com>
2020-04-17 19:39:18 -0400

    added color to task accuracy and auto scale for time

--- js/analysis.js
+++ js/analysis.js
@@ -29,8 +29,12 @@ function makePlots(provData) {
   }
 
 
-  console.log(provData)
+  let maxTime = provData.reduce((acc,currValue)=> currValue.totalStudyTime > acc? currValue.totalStudyTime : acc,0)+200000;
   var margin = { top: 50, right: 15, bottom: 25, left: 150 };
+
+  let color = d3.scaleLinear()
+  .domain([0,1])
+  .range(['red','blue'])
 
   var height = 180;
   var width = (window.screen.availWidth - margin.left - margin.right)/2 ;
@@ -100,7 +104,7 @@ function makePlots(provData) {
 
   var x = d3.scaleLinear().range([0, width]);
 
-  x.domain([0, 40 * 60 * 1000]);
+  x.domain([0, maxTime]);
 
   var y = d3.scaleLinear().range([height - 10, 0]);
   y.domain([-2, 2]); //provData[index].provEvents.filter(e=>e.type === type && e.level === undefined).length-1+2]);
@@ -211,7 +215,8 @@ function makePlots(provData) {
       d.task && d.task.data && d.task.data.answer
         ? d.task.data.answer.correct == 0
         : false
-    );
+    )
+    .attr('fill',d=>(d.task && d.task.result) ? color(d.task.result.accuracy) : '')
   // .classed('sortedOn', d=>sortOrder && d.task && d.task.id == sortOrder)
 
   rects
